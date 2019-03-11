baa111042dd5792757019cde9344eb82c012e6e2
Natalya Gulyaeva <n.gulyaeva@iz.ru>
2019-03-11 12:10:23 +0300

    видео в webgl: каждый раз заново

--- connectedScroll_unity3d/Assets/Scr/Active.cs
+++ connectedScroll_unity3d/Assets/Scr/Active.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 public class Active : MonoBehaviour
 {
     [SerializeField] GameObject _active;
+    [SerializeField] GameObject _videoPrefab;
+
+    private GameObject _video;
 
     private void Start()
     {
@@ -13,10 +16,15 @@ public class Active : MonoBehaviour
     private void Show(Target target)
     {
         _active.SetActive(true);
+        _video = Instantiate(_videoPrefab);
     }
 
     public void Hide()
     {
         _active.SetActive(false);
+        if (_video != null)
+        {
+            Destroy(_video);
+        }
     }
 }
